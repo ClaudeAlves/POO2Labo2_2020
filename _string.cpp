325e9ba4d3e50025d163751cd65af9abc8b538f4
ClaudeAlves <claudeuw@hotmail.com>
2020-03-12 17:15:08 +0100

    fichiers de base

---
+++ _string.cpp
@@ -0,0 +1,5 @@
+//
+// Created by claudealves on 12.03.20.
+//
+
+#include "_string.h"
---
+++ _string.h
@@ -0,0 +1,14 @@
+//
+// Created by claudealves on 12.03.20.
+//
+
+#ifndef POO2LABO2_2020__STRING_H
+#define POO2LABO2_2020__STRING_H
+
+
+class _string {
+
+};
+
+
+#endif //POO2LABO2_2020__STRING_H
