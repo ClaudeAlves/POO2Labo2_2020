ca373e0e76bfbfb1b32262674911f8d45aeef2b0
ClaudeAlves <claudeuw@hotmail.com>
2020-03-12 17:44:27 +0100

    squelette des fonctions

--- _string.cpp
+++ _string.cpp
@@ -3,3 +3,111 @@
 //
 
 #include "_string.h"
+
+_string::_string() {
+
+}
+
+_string::_string(const _string &other) {
+
+}
+
+_string::_string(const char *const &other) {
+
+}
+
+_string::_string(const std::string &other) {
+
+}
+
+size_t _string::size() const {
+    return 0;
+}
+
+char &_string::at(size_t i) {
+    return <#initializer#>;
+}
+
+char &_string::operator()(size_t i) {
+    return <#initializer#>;
+}
+
+bool _string::operator==(const _string &other) const {
+    return false;
+}
+
+bool _string::operator==(const char *const &other) const {
+    return false;
+}
+
+bool _string::operator==(const std::string &other) const {
+    return false;
+}
+
+bool _string::equal(const _string &other) const {
+    return false;
+}
+
+bool _string::equal(const char *const &other) const {
+    return false;
+}
+
+bool _string::equal(const std::string &other) const {
+    return false;
+}
+
+_string &_string::operator=(const _string &other) {
+    return <#initializer#>;
+}
+
+_string &_string::operator=(const char *const &other) {
+    return <#initializer#>;
+}
+
+_string &_string::operator=(const std::string &other) {
+    return <#initializer#>;
+}
+
+_string &_string::set(const _string &other) {
+    return <#initializer#>;
+}
+
+_string &_string::set(const char *const &other) {
+    return <#initializer#>;
+}
+
+_string &_string::set(const std::string &other) {
+    return <#initializer#>;
+}
+
+_string &_string::append(const _string &other) {
+    return <#initializer#>;
+}
+
+_string &_string::append(const char *const &other) {
+    return <#initializer#>;
+}
+
+_string &_string::append(const std::string &other) {
+    return <#initializer#>;
+}
+
+_string &_string::operator+=(const _string &other) {
+    return <#initializer#>;
+}
+
+_string &_string::operator+=(const char *const &other) {
+    return <#initializer#>;
+}
+
+_string &_string::operator+=(const std::string &other) {
+    return <#initializer#>;
+}
+
+_string &_string::subString(int begin, int end) const {
+    return <#initializer#>;
+}
+
+std::ostream &operator<<(std::ostream &out, const _string &_string) {
+    return <#initializer#>;
+}
--- _string.h
+++ _string.h
@@ -6,7 +6,68 @@
 #define POO2LABO2_2020__STRING_H
 
 
+#include <cstdlib>
+#include <string>
+
 class _string {
+public :
+    _string();
+
+    _string(const _string &other);
+
+    _string(const char *const &other);
+
+    _string(const std::string &other);
+
+    size_t size() const;
+
+    char &at(size_t i);
+
+    char &operator()(size_t i);
+
+    bool operator==(const _string &other) const;
+
+    bool operator==(const char *const &other) const;
+
+    bool operator==(const std::string &other) const;
+
+    bool equal(const _string &other) const;
+
+    bool equal(const char *const &other) const;
+
+    bool equal(const std::string &other) const;
+
+    _string &operator=(const _string &other);
+
+    _string &operator=(const char *const &other);
+
+    _string &operator=(const std::string &other);
+
+    _string &set(const _string &other);
+
+    _string &set(const char *const &other);
+
+    _string &set(const std::string &other);
+
+    _string &append(const _string &other);
+
+    _string &append(const char *const &other);
+
+    _string &append(const std::string &other);
+
+    _string &operator+=(const _string &other);
+
+    _string &operator+=(const char *const &other);
+
+    _string &operator+=(const std::string &other);
+
+    _string &subString(int begin, int end) const;
+
+    friend std::ostream &operator<<(std::ostream &out, const _string &_string);
+
+private :
+    const char *content;
+    size_t length;
 
 };
 
